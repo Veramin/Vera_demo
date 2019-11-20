505bbe5dccc28e9310f17f249e23cc88e20ec40a
vera_min <vera_min@163.com>
2019-11-20 18:09:05 +0800

    引入并使用组件

--- reactStudy_demo/hello-world/src/App.js
+++ reactStudy_demo/hello-world/src/App.js
@@ -1,22 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Greet from './components/greet'
+// import stateful class components
+import Welcome from './components/welcome'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello, World.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App">
+          <Greet></Greet>
+          <Welcome />
     </div>
   );
 }
