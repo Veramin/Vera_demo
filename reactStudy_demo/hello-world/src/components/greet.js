d99868cbff2cb825c5f99f58554639e65ac077f7
vera_min <vera_min@163.com>
2019-11-20 18:10:10 +0800

    创建了新的组件

---
+++ reactStudy_demo/hello-world/src/components/greet.js
@@ -0,0 +1,13 @@
+/* stateless functional components */
+import React from 'react'
+
+// ES5
+// function Greet() {
+//     return <h1>Hello, vera.</h1>
+// }
+
+// ES6
+const Greet = () => <h1>Hello, vera.............</h1>
+
+
+export default Greet;
---
+++ reactStudy_demo/hello-world/src/components/welcome.js
@@ -0,0 +1,11 @@
+/* stateful class components */
+import React, { Component } from 'react'
+
+class Welcome extends Component {
+    render() {
+        return <h1>Welcome to the react's world.</h1>
+    }
+}
+
+// export component
+export default Welcome;
